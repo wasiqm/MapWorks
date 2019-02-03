f4d5f4d14f18ff6b28d961ad1e4db6f7c37e26d5
Saskia Gilmer <sasasquatch@Saskias-MacBook-Air.local>
2019-02-02 20:10:06 -0500

    removed controls

--- src/GoogleMaps.js
+++ src/GoogleMaps.js
@@ -4,13 +4,14 @@ import {Map, InfoWindow, Marker, GoogleApiWrapper} from 'google-maps-react';
 export class MapContainer extends React.Component {
     render() {
       return (
-        <Map google={this.props.google} zoom={14}>
-
+        <Map google={this.props.google} mapTypeControl = {false} streetViewControl = {false} fullscreenControl = {false} zoom={14}>
         </Map>
+        
       );
     }
   }
  
 export default GoogleApiWrapper({
-  apiKey: 'AIzaSyD0CqwnsGxlFN4GKzegF6cvHfD1Cjzj8OM'
+  apiKey: 'AIzaSyD0CqwnsGxlFN4GKzegF6cvHfD1Cjzj8OM',
+  mapTypeControl: false
 })(MapContainer)
