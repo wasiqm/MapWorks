cf2b4e4756a54a7e44ff2ec41a5d6de4b2088886
Tokery <kevincott97@gmail.com>
2019-02-02 22:42:22 -0500

    commit

--- src/GoogleMaps.js
+++ src/GoogleMaps.js
@@ -1,26 +1,51 @@
 import React from 'react';
-import {Map, GoogleApiWrapper, Marker } from 'google-maps-react';
+import {Map, GoogleApiWrapper, Marker, InfoWindow } from 'google-maps-react';
 
 export class MapContainer extends React.Component {
-    state = { center: null };
-    componentDidMount() {
-      if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(position => {
-          this.setState({ center: { 
-            lat: position.coords.latitude,
-            lng: position.coords.longitude
-          }});
-        });
-      }
-    }  
+  state = { 
+    center: null,
+    activeMarker: null,
+    showingInfoWindow: false,
+  };
+  componentDidMount() {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(position => {
+        this.setState({ center: { 
+          lat: position.coords.latitude,
+          lng: position.coords.longitude
+        }});
+      });
+    }
+  }  
+
+  onMarkerClick = (props, marker, e) => {
+    this.setState({
+      selectedPlace: props,
+      activeMarker: marker,
+      showingInfoWindow: true
+    });
+  }
+ 
+  onMapClicked = (props) => {
+    if (this.state.showingInfoWindow) {
+      this.setState({
+        showingInfoWindow: false,
+        activeMarker: null
+      })
+    }
+  };
 
     render() {
       const points = [
-        {location:[45.500089, -73.564600], weight: 2}
+        {location:[45.500089, -73.564600], weight: 1},
+        {location:[45.500088, -73.564600], weight: 1},
+        {location:[45.500087, -73.564600], weight: 1},
+        {location:[45.500086, -73.564600], weight: 1}
       ];
       return (
         <Map 
           google={this.props.google} 
+          onClick={this.onMapClicked}
           zoom={14}
           initialCenter={{
             lat: 45.500089,
@@ -28,23 +53,30 @@ export class MapContainer extends React.Component {
           }}
           center={this.state.center}
           yesIWantToUseGoogleMapApiInternals
-          google={this.props.google}
           mapTypeControl = {false}
           streetViewControl = {false}
           fullscreenControl = {false}
-          zoom={14}
           onGoogleApiLoaded={({map, maps}) => {
             const heatmap = new maps.visualization.HeatmapLayer({
               data: points.map(point => (
                 {location: new maps.LatLng(point['location'][1], point['location'][0]),
-                weight: point['weight']}))
+                weight: point['weight']})),
+              radius: 20,
             });
             heatmap.setMap(map);
           }}
         >
           <Marker 
             name="Test"
+            onClick={this.onMarkerClick}
           />
+          <InfoWindow
+            marker={this.state.activeMarker}
+            visible={this.state.showingInfoWindow}>
+              <div>
+                <h1>Kevin</h1>
+              </div>
+          </InfoWindow>
         </Map>
         
       );
